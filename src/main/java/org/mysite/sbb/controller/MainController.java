0716e85f1ced7018b6f73f5d183c7e6a7c84f2ab
HyeonGi2 <tmskc56@gmail.com>
2025-09-01 19:13:38 +0900

    메인 컨트롤러에서 루트페이지 설정

--- src/main/java/org/mysite/sbb/controller/MainController.java
+++ src/main/java/org/mysite/sbb/controller/MainController.java
@@ -1,3 +1,9 @@
+/*------------------------
+250901 홍현기
+8080 url 호출 시 404 오류페이지가 아닌 질문 목록 화면 출력을 위해 수정.
+기본화면은 root라 칭하고 이 프로젝트는 질문 목록을 기본페이지로한다.
+
+-------------------------*/
 package org.mysite.sbb.controller;
 
 import org.springframework.stereotype.Controller;
@@ -11,4 +17,10 @@ public class MainController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
 }
