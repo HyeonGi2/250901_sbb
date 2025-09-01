4628f43f827c2a0e57caccb66dc437389834ca0e
HyeonGi2 <tmskc56@gmail.com>
2025-09-01 17:21:26 +0900

    질문 및 답변 엔티티 생성

---
+++ src/main/java/org/mysite/sbb/domain/Answer.java
@@ -0,0 +1,32 @@
+/*-----------------------
+250901 홍현기
+domain 또는 답변 entity 작성
+엔티티로 테이블 매핑
+
+-------------------------*/
+
+package org.mysite.sbb.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    //ManyToOne 추가하기 하나의 질문에 여러개의 답변이므로 ManyToOne 반대는 OneToMany
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/org/mysite/sbb/domain/Question.java
@@ -0,0 +1,38 @@
+/*-----------------------
+250901 홍현기
+domain 또는 질문 entity 작성
+엔티티로 테이블 매핑
+
+-------------------------*/
+package org.mysite.sbb.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+// 엔티티로 만들기 위해 @Entity 적용
+@Entity
+public class Question {
+
+    // id 질문 고유번호, sub 질문 제목, content 질문 내용, createDate 질문 작성 일시
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //(고유번호, 순서대로 1씩증가)
+    private Integer id;
+
+    @Column(length = 200) //200자 제한
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") //제한 x
+    private String content;
+
+    private LocalDateTime createDate;
+
+    //질문에서 답변 참조, 질문 삭제시 답변도 모두 삭제되도록 cascade = CascadeType.REMOVE 사용. https://www.baeldung.com/jpa-cascade-types
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answersList;
+}
