45f568a4c6141bc9e5683508f7f32208c0b6f220
HyeonGi2 <tmskc56@gmail.com>
2025-09-30 13:32:12 +0900

    상세페이지 만들기 및 출력하기

--- src/main/java/org/mysite/sbb/service/QuestionService.java
+++ src/main/java/org/mysite/sbb/service/QuestionService.java
@@ -1,11 +1,13 @@
 package org.mysite.sbb.service;
 
 import lombok.RequiredArgsConstructor;
+import org.mysite.sbb.DataNotFoundException;
 import org.mysite.sbb.dao.QuestionRepository;
 import org.mysite.sbb.domain.Question;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,6 +17,15 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 
 }
