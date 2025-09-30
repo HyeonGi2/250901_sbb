46dc481e44ff55b12a67188b6494a2ab5847cb47
HyeonGi2 <tmskc56@gmail.com>
2025-09-30 16:00:34 +0900

    질문 등록 기능 추가 중 오류 발생

--- src/main/java/org/mysite/sbb/dao/QuestionRepository.java
+++ src/main/java/org/mysite/sbb/dao/QuestionRepository.java
@@ -13,5 +13,6 @@ import org.mysite.sbb.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
+    Question findBySubject(String subject);
 
 }
--- src/test/java/org/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/org/mysite/sbb/SbbApplicationTests.java
@@ -6,7 +6,10 @@ import org.mysite.sbb.domain.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -17,16 +20,11 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
+        assertEquals(2, this.questionRepository.count());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
 }
